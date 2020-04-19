87e106ea60844d4df71db73d0dcc083da5a3457b
xaviergregor <xavier@gregor.fr>
2020-04-19 14:23:51 +0200

    changement de nom

--- fail2banGeoIP.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo fail2ban-client status sshd | grep "Banned IP list:" | sed 's/`- Banned IP list://g' | xargs -n 1 geoiplookup | sed 's/GeoIP Country Edition://g'
-
--- topiplocation.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-## all Fail2Ban Log should be logged into /var/log/fail2ban.log then this will work
-## you need geoiplookup to get it runningyou can install it with sudo apt install geoip-bin
-cat /var/log/fail2ban.log* | grep Ban | sed 's/.*[Bb]an \(.*\)/\1/' | uniq | while read line; do geoiplookup $line; done | sort | uniq -c | sort -nr
