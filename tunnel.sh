ae49fcd3c5103963a95b0e50ffe6071e664c7ac6
xaviergregor <xavier@gregor.fr>
2021-03-06 19:31:07 +0100

    tunnel SSH remote

---
+++ tunnel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SSH_REDIR_PORT=10000
+SSH_D="tunnel@defcon2.duckdns.org -p 1022"
+COMMAND="ssh -R $SSH_REDIR_PORT:localhost:22 $SSH_D -N -f"
+pgrep -f "ssh -R $SSH_REDIR_PORT:localhost:22" > /dev/null 2>&1 || (eval $COMMAND && echo $COMMAND)
