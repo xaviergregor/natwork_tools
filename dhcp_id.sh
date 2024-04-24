2e525c15fbb1527ec3950fe599cf3dcb10ff20b3
xaviergregor <xavier@gregor.fr>
2024-04-24 09:34:49 +0200

    update script

--- dhcp_id.sh
+++ dhcp_id.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-
+#Script pour detecter le serveur DHCP sur le réseau
+#
+# Author: Xavier Gregor
+# Contact: Github issue
+# Version 0.1
+#
+# Copyright (c) 2024 Xavier Gregor
+#
 # Fonction pour obtenir l'IP du serveur DHCP sur macOS
+
 get_dhcp_mac() {
     ipconfig getpacket en0 | awk '/server_identifier/ {print $3}'
 }
 
 # Fonction pour obtenir l'IP du serveur DHCP sur Linux
+
 get_dhcp_linux() {
     grep 'dhcp-server-identifier' /var/lib/dhcp/dhclient.*.leases | tail -1 | awk '{print $3}' | sed 's/;//'
 }
 
 # Détecter le système d'exploitation
+
 OS=$(uname -s)
 case "$OS" in
     "Darwin") # macOS
